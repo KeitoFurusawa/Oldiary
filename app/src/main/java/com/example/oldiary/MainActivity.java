cbe3eeae4f6bbd0c5ccb7f0d5cfc0925777f5a1f
KeitoFurusawa <wisdomsound1.0.0@gmail.com>
2022-10-19 15:39:56 +0900

    imade new branch test01

--- app/src/main/java/com/example/oldiary/MainActivity.java
+++ app/src/main/java/com/example/oldiary/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.oldiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+
+//this is test01 branch
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        ImageView img = findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.od_logo);
     }
 }
