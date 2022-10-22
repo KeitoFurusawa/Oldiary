baa580ce6e54d4d816ca8d148663125e53819f7f
Shogo Abe <s1F102001333@iniad.org>
2022-10-22 23:06:48 +0900

    画面遷移テスト

--- app/src/main/java/com/example/oldiary/MainActivity.java
+++ app/src/main/java/com/example/oldiary/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.oldiary;
+// https://akira-watson.com/android/activity-1.html←参考
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //アクションバー非表示
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
+        Button sendButton = findViewById(R.id.send_button);
+        // lambda式
+        sendButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), SubActivity.class);
+            startActivity(intent);
+        });
     }
 }
-
-// test
