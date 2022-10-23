aec502ca2683d3905449ddb704c5511255635cc0
Shogo Abe <s1F102001333@iniad.org>
2022-10-24 01:12:40 +0900

    BGM挿入

--- app/src/main/java/com/example/oldiary/MainActivity.java
+++ app/src/main/java/com/example/oldiary/MainActivity.java
@@ -2,18 +2,42 @@ package com.example.oldiary;
 // https://akira-watson.com/android/activity-1.html←参考
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
-import android.widget.Button;
 import android.view.MotionEvent;
-import android.widget.TextView;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.media.AudioAttributes;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.talkative);
+        mediaPlayer.setLooping(true);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.pause();
+    }
+
+    protected void onDestry() {
+        super.onDestroy();
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
 
     public boolean onTouchEvent(MotionEvent event) {
@@ -25,4 +49,3 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 }
-// comment
