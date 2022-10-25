c0b1c7739337bdbe403b9fba3bb6baf0dde7bd8e
Yuta Uchida <s1f102000859@iniad.org>
2022-10-26 00:17:44 +0900

    ボタン関数外部化

--- app/src/main/java/com/example/oldiary/Create2Activity.java
+++ app/src/main/java/com/example/oldiary/Create2Activity.java
@@ -1,9 +1,10 @@
 package com.example.oldiary;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Create2Activity extends AppCompatActivity {
 
@@ -12,13 +13,18 @@ public class Create2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create2);
 
+        setOnClick();
+        setOnClick2();
+    }
+    protected void setOnClick() {
         Button button = findViewById(R.id.button13);
         // lambda式
         button.setOnClickListener(v -> {
             Intent intent = new Intent(getApplication(), CreateActivity.class);
             startActivity(intent);
         });
-
+    }
+    protected void setOnClick2() {
         Button button2 = findViewById(R.id.button14);
         // lambda式
         button2.setOnClickListener(v -> {
