f775b8e1f2720bb369a380b43514bb3789c4b810
Shogo Abe <s1F102001333@iniad.org>
2022-10-27 00:43:18 +0900

    homeに音楽

--- app/src/main/java/com/example/oldiary/HomeActivity.java
+++ app/src/main/java/com/example/oldiary/HomeActivity.java
@@ -2,15 +2,29 @@ package com.example.oldiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ensolarado);
+        mediaPlayer.setLooping(true);
     }
 
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
+    }
 
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.pause();
+    }
 }
