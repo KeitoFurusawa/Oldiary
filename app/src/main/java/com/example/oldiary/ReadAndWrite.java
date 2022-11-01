60e32fa975c05ffcf749f651ef979125233893ce
KeitoFurusawa <110015118+KeitoFurusawa@users.noreply.github.com>
2022-11-01 18:51:24 +0900

    fixed few things

--- app/src/main/java/com/example/oldiary/ReadAndWrite.java
+++ app/src/main/java/com/example/oldiary/ReadAndWrite.java
@@ -2,8 +2,11 @@ package com.example.oldiary;
 
 import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ReadAndWrite {
     private static final String TAG = "CreateActivity";
@@ -26,23 +29,39 @@ public class ReadAndWrite {
         String ID = sb.toString();
         Log.d(TAG, ID);
         mDatabase.child("users").child(ID).setValue(user);
-    }
-/*
-    // データベースから一度だけ情報を読み取る
-    public void getData(String userId) {
-        mDatabase.child("users").child(userId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
-                    Log.e("firebase", "Error getting data", task.getException());
-                }
-                else {
 
-                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
-                }
-            }
-        });
+        addEventListener(mDatabase);
+        // login画面でチェックする
     }
-*/
+
+    // パスにあるデータを読み取って、変更をリッスンするには、addValueEventListener() メソッドを使用して
+    // ValueEventListener を DatabaseReference に追加します。
+    public void addEventListener(DatabaseReference mPostReference) {
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override   // 特定のパスにあるコンテンツの静的スナップショットを、イベントの発生時に存在していたとおりに読み取ることができる
+            // Listenerがアタッチされたときに1回、また、データ(子も含む)が変更されるたびにコールされる
+            /**
+             * @param dataSnapshot:その場所にあるすべてのデータ（子のデータも含む）を含んでいるスナップショット
+             */
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                /**
+                 * Snapshot.getValue():そのデータの Java オブジェクト表現が返され、その場所にデータが存在しない場合、null が返される。
+                 */
+                //Post post = dataSnapshot.getValue(Post.class);
+
+                // realtime databaseから情報を読み取る
+                String email = (String)dataSnapshot.child("users").child("1").child("email").getValue();
+                String username = (String)dataSnapshot.child("users").child("1").child("username").getValue();
+                Log.i(TAG, "email:" + email);
+                Log.i(TAG, "username:" + username);
+            }
+
+            @Override   // 読み取りがキャンセルされた場合に呼び出される
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        mPostReference.addValueEventListener(postListener);
+    }
 
 }
