7b106e75ca4f13b090d175db9e3b504357b5c28d
KeitoFurusawa <110015118+KeitoFurusawa@users.noreply.github.com>
2022-11-03 01:24:48 +0900

    merging now

--- app/src/main/java/com/example/oldiary/HomeActivity.java
+++ app/src/main/java/com/example/oldiary/HomeActivity.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.content.Intent;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-
     MediaPlayer mediaPlayer;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Intent intent1 = getIntent();
+        userId = intent1.getStringExtra("UserID");
+        TextView textViewUserId = findViewById(R.id.textViewId);
+        textViewUserId.setText(userId);
 
         playMusic();
         setOnClick();
