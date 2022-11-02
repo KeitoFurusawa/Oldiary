16672fa6d43da2b4779754eae059c4a62dfa6c60
KeitoFurusawa <110015118+KeitoFurusawa@users.noreply.github.com>
2022-11-03 01:22:18 +0900

    merging now

--- app/src/main/java/com/example/oldiary/HomeActivity.java
+++ app/src/main/java/com/example/oldiary/HomeActivity.java
@@ -3,7 +3,8 @@ package com.example.oldiary;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
-import android.widget.Button;
+import android.content.Intent;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,8 +15,18 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        playMusic();
+        setOnClick();
+        setOnClick2();
+
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.home_music);
         mediaPlayer.setLooping(true);
+    }
+
+    protected void playMusic() {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ensolarado);
+        mediaPlayer.setLooping(true);
+
     }
 
     protected void onResume() {
@@ -26,5 +37,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mediaPlayer.pause();
+    }
+
+    protected void setOnClick() {
+        ImageButton button = findViewById(R.id.back_start);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    protected void setOnClick2() {
+        ImageButton button2 = findViewById(R.id.write);
+        button2.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), WriteActivity.class);
+            startActivity(intent);
+        });
     }
 }
