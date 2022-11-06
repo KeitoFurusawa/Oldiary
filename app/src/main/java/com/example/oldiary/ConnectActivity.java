1e0a56a411debd751a977d0beed88b26d1b913da
Shogo Abe <s1F102001333@iniad.org>
2022-11-07 00:24:00 +0900

    みんなの投稿

--- app/src/main/java/com/example/oldiary/ConnectActivity.java
+++ app/src/main/java/com/example/oldiary/ConnectActivity.java
@@ -3,13 +3,9 @@ package com.example.oldiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 public class ConnectActivity extends AppCompatActivity {
 
@@ -25,7 +21,7 @@ public class ConnectActivity extends AppCompatActivity {
 
 
         setOnClick();
-        changePhoto();
+
     }
 
     protected void setOnClick() {
@@ -52,23 +48,4 @@ public class ConnectActivity extends AppCompatActivity {
         mediaPlayer = null;
     }
 
-    protected void changePhoto() {
-        Button button = findViewById(R.id.changePhoto);
-        button.setOnClickListener(v -> {
-            // ImageViewを変数化
-            ImageView albumArt = findViewById(R.id.random_photo);
-
-            // ランダム画像のリソースの配列を宣言
-            TypedArray typedArray = getApplicationContext().getResources().obtainTypedArray(R.array.history_arrays);
-
-            // ランダムな数値を設定 (*今回は配列の長さが3のため、3を指定)
-            int rand = (int)(Math.floor(Math.random() * 3));
-
-            // ランダムで画像を選択する
-            Drawable drawable = typedArray.getDrawable(rand);
-
-            // ImageViewの画像の値を設定
-            albumArt.setImageDrawable(drawable);
-        });
-    }
 }
