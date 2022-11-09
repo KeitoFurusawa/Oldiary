f0f13cc8e36cfef965d0c3e7293feec6c155a265
Yuta Uchida <s1f102000859@iniad.org>
2022-11-09 14:59:43 +0900

    photo

--- app/src/main/java/com/example/oldiary/WriteActivity.java
+++ app/src/main/java/com/example/oldiary/WriteActivity.java
@@ -1,16 +1,54 @@
 package com.example.oldiary;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class WriteActivity extends AppCompatActivity {
+
+    static final int REQUEST_CAPTURE_IMAGE = 100;
+
+    Button button1;
+    ImageView imageView1;
+
+
+    private static final int RESULT_PICK_IMAGEFILE = 1000;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        findViews();
+        setListeners();
+
+        imageView = (ImageView)findViewById(R.id.image_view);
+
+        findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
+            }
+        });
 
         setOnClick();
     }
@@ -25,4 +63,54 @@ public class WriteActivity extends AppCompatActivity {
         });
     }
 
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+    protected void findViews(){
+        button1 = (Button)findViewById(R.id.button3);
+        imageView1 = (ImageView)findViewById(R.id.imageView);
+    }
+    protected void setListeners(){
+        button1.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(
+                        intent,
+                        REQUEST_CAPTURE_IMAGE);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+
+        if (REQUEST_CAPTURE_IMAGE == requestCode
+                && resultCode == Activity.RESULT_OK) {
+            Bitmap capturedImage =
+                    (Bitmap) resultData.getExtras().get("data");
+            imageView1.setImageBitmap(capturedImage);
+        }
+        if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+
+                try {
+                    Bitmap bmp = getBitmapFromUri(uri);
+                    imageView.setImageBitmap(bmp);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
 }
