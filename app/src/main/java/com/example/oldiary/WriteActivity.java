e66f2bdfebcab977ecedba25ade9b780d61a3454
KeitoFurusawa <110015118+KeitoFurusawa@users.noreply.github.com>
2022-11-11 04:18:03 +0900

    投稿だけできる

--- app/src/main/java/com/example/oldiary/WriteActivity.java
+++ app/src/main/java/com/example/oldiary/WriteActivity.java
@@ -1,11 +1,16 @@
 package com.example.oldiary;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class WriteActivity extends AppCompatActivity {
@@ -21,6 +26,7 @@ public class WriteActivity extends AppCompatActivity {
         userId =  preference.getString("UserID", "");
         checkUserId();
         setOnClick();
+        setOnClickPost();
     }
 
     protected void checkUserId() {
@@ -41,4 +47,26 @@ public class WriteActivity extends AppCompatActivity {
         });
     }
 
+    protected void setOnClickPost() {
+        Button btnPost = findViewById(R.id.post);
+        btnPost.setOnClickListener(v -> {
+            TextView textView = findViewById(R.id.editTextTextMultiLine2);
+            String txt = textView.getText().toString();
+            ReadAndWrite rad = new ReadAndWrite();
+            rad.writeDiary(userId, txt);
+            new AlertDialog.Builder(WriteActivity.this)
+                    .setTitle("")
+                    .setMessage("投稿されました！")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // OKボタン押下時の処理
+                            Intent intent = new Intent(getApplication(), HomeActivity.class);
+                            startActivity(intent);
+                            Log.d("AlertDialog", "Positive which :" + which);
+                        }
+                    })
+                    .show();
+
+        });
+    }
 }
