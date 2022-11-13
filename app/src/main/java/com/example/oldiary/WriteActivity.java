5674fa3b02dfd654797228f33663800959f3673d
Yuta Uchida <s1f102000859@iniad.org>
2022-11-14 01:12:49 +0900

    photo1

--- app/src/main/java/com/example/oldiary/WriteActivity.java
+++ app/src/main/java/com/example/oldiary/WriteActivity.java
@@ -2,16 +2,26 @@ package com.example.oldiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class WriteActivity extends AppCompatActivity {
     private final static String TAG = "home";
@@ -68,5 +78,54 @@ public class WriteActivity extends AppCompatActivity {
                     .show();
 
         });
+    }
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+    protected void findViews(){
+        button1 = findViewById(R.id.button3);
+    }
+    protected void setListeners(){
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(
+                        intent,
+                        REQUEST_CAPTURE_IMAGE);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_CAPTURE_IMAGE
+                && resultCode == Activity.RESULT_OK) {
+            Bitmap capturedImage =
+                    (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(capturedImage);
+        }
+        if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == RESULT_OK) {
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+
+                try {
+                    Bitmap bmp = getBitmapFromUri(uri);
+                    imageView.setImageBitmap(bmp);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
     }
 }
