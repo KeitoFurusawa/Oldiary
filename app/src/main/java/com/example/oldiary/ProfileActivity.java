d67fc4cedd52f523639251d3e662f8b00c9055ee
Shogo Abe <s1F102001333@iniad.org>
2022-11-16 15:18:49 +0900

    用語追加

--- app/src/main/java/com/example/oldiary/ProfileActivity.java
+++ app/src/main/java/com/example/oldiary/ProfileActivity.java
@@ -1,17 +1,28 @@
 package com.example.oldiary;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+    AlertDialog.Builder dialogBuilder;
+    Dialog dialog;
+    EditText user_name, content1, content2, content3, comment;
+    Button save, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,7 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         setOnClickBack();
+        editProfile();
 
     }
 
@@ -28,5 +40,18 @@ public class ProfileActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplication(), HomeActivity.class);
             startActivity(intent);
         });
+    }
+
+    protected void editProfile() {
+        Button button = findViewById(R.id.button2);
+        dialog = new Dialog(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.setContentView(R.layout.popup_prof);
+                final View contactPopupView = getLayoutInflater().inflate(R.layout.popup_prof, null);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/oldiary/WordActivity.java
+++ app/src/main/java/com/example/oldiary/WordActivity.java
@@ -2,11 +2,14 @@ package com.example.oldiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
 public class WordActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,22 @@ public class WordActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.collection_of_words);
         textView.setMovementMethod(new ScrollingMovementMethod());
+
+        playMusic();
+    }
+
+    protected void playMusic() {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.studyingmusic);
+        mediaPlayer.setLooping(true);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.pause();
     }
 }
