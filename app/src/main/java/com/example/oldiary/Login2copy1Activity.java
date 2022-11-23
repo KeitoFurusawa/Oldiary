e7d445f40e4fd12ce7fe54a411c025ba46d275a3
Yuta Uchida <s1f102000859@iniad.org>
2022-11-23 15:44:10 +0900

    暗証番号複製

---
+++ app/src/main/java/com/example/oldiary/Login2copy1Activity.java
@@ -0,0 +1,14 @@
+package com.example.oldiary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login2copy1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2copy1);
+    }
+}
