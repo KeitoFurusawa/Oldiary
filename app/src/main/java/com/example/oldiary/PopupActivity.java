98c56dc753c32e902583fec0f37ef42ca2932054
KeitoFurusawa <110015118+KeitoFurusawa@users.noreply.github.com>
2022-12-04 01:29:56 +0900

    プロフィール画面でデータ反映 ボタンの有効化

--- app/src/main/java/com/example/oldiary/PopupActivity.java
+++ app/src/main/java/com/example/oldiary/PopupActivity.java
@@ -1,11 +1,29 @@
 package com.example.oldiary;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+
 public class PopupActivity extends AppCompatActivity {
+    private static final String TAG = "prof";
+    private DatabaseReference mDatabase;
+    private SharedPreferences preference;
+    private SharedPreferences.Editor editor;
+    private String userId;
+    private String userName;
+    private String comment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +33,61 @@ public class PopupActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+        setOnClick();
+        getUserId();
+        setUserName();
+        setComment();
+    }
+
+    protected void getUserId() {
+        userId = preference.getString("UserID", "");
+    }
+
+    private void setUserName() {
+        mDatabase.child("users").child(userId).child("userName").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e(TAG, "Error getting data", task.getException());
+                }
+                else {
+                    userName = String.valueOf(task.getResult().getValue());
+                    TextView textUserName = findViewById(R.id.textViewUserName);
+                    textUserName.setText(userName);
+                }
+            }
+        });
+    }
+
+    private void setComment() {
+        mDatabase.child("users").child(userId).child("comment").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e(TAG, "Error getting data", task.getException());
+                }
+                else {
+                    comment = String.valueOf(task.getResult().getValue());
+                    if (comment.equals("null")) {
+                        Log.d(TAG, "the result of getting comment data is null");
+                    } else {
+                        TextView textComment = findViewById(R.id.comment);
+                        textComment.setText(userName);
+                    }
+                }
+            }
+        });
+    }
+
+    private void setOnClick() {
+        Button btnCancel = findViewById(R.id.cancel);
+        Button btnNext = findViewById(R.id.next);
+        btnCancel.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), ProfileActivity.class);
+            startActivity(intent);
+        });
+        btnNext.setOnClickListener(v -> {
+
+        });
     }
 }
